7d0f2cde80c9a23d58ab1e81d7c594d8373b89ec
Muhammet Bulut <83714453+bunshiin@users.noreply.github.com>
2023-06-09 17:35:18 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function togglePopup() {
+    var popup = document.getElementById("popup-1");
+    popup.classList.toggle("active");
+}
+
+function closePopup() {
+    var popup = document.getElementById("popup-1");
+    popup.classList.remove("active");
+}
+
+
+
